414d8c6f44661b18a29294ea071bd541e4b9e1f2
FIC <lucas_redigolo@hotmail.com>
2017-10-11 19:10:21 -0300

    inclui

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PJT_11_Outubro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
