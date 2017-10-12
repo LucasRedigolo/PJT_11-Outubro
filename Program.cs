beb6e2b628cbcfdc6c96bc41c7acb4ff42e54cc2
FIC <lucas_redigolo@hotmail.com>
2017-10-11 21:14:08 -0300

    add nome

--- Program.cs
+++ Program.cs
@@ -6,11 +6,26 @@ namespace PJT_11_Outubro
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            int sair = 1;
+            bool ConverteuSair = false;
             string[] perguntas = File.ReadAllLines(@"perguntas.txt");
             string[] respostas = new string[perguntas.Length];
 
-            StreamWriter respostasSaida = new StreamWriter("respostas.csv");
+            StreamWriter respostasSaida = new StreamWriter("respostas.csv", true);
+
+            for (int i=0; i < perguntas.Length; i++)
+
+            {
+
+            respostasSaida.Write(perguntas[i]);
+            respostasSaida.Write("; ");
+
+            }
+
+            respostasSaida.WriteLine();
+
+            do{
 
             for (int i=0; i < perguntas.Length; i++)
             {
@@ -18,8 +33,18 @@ namespace PJT_11_Outubro
                 respostas[i] = Console.ReadLine();
                 respostasSaida.Write(respostas[i]);
                 respostasSaida.Write("; ");
+
             }
 
+            respostasSaida.WriteLine();
+            Console.WriteLine("\nDigite 1 para inserir novo cadastro ou aperte qualquer tecla+Enter para finalizar.");
+            ConverteuSair = int.TryParse(Console.ReadLine(), out sair);
+            Console.WriteLine();
+
+
+            }while (sair == 1);
+            
+            Console.WriteLine("Cadastro Finalizado!!");
             respostasSaida.Close();
         }
     }
